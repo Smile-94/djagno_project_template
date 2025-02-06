4e4f1d9f7383ba3a41792337d55d13aef4c72149
Sazzad Hossen <mshossen75@gmail.com>
2025-02-06 23:35:02 +0600

    feat: set up djagno

---
+++ config/django/database.py
@@ -0,0 +1,74 @@
+from enum import Enum
+from typing import Any
+
+from pydantic import Field, SecretStr, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from config.env import env_config
+
+
+class DatabaseChoices(Enum):
+    POSTGRES = "postgres"
+    MYSQL = "mysql"
+    SQLITE = "sqlite3"
+
+
+class DatabaseSettings(BaseSettings):
+    """
+    This class defines the setting configuration for this auth service
+    """
+
+    # General Database Configuration
+    DEFAULT_AUTO_FIELD: str = "django.db.models.BigAutoField"
+
+    # PostgreSQL Database Configuration
+    POSTGRES_HOST: SecretStr = Field(default="localhost", frozen=True, repr=False)
+    POSTGRES_PORT: int = Field(default=5432, frozen=True, repr=False)
+    POSTGRES_DB: SecretStr = Field(default="postgres", frozen=True, repr=False)
+    POSTGRES_USER: SecretStr = Field(default="postgres", frozen=True, repr=False)
+    POSTGRES_PASSWORD: SecretStr = Field(default="postgres", frozen=True, repr=False)
+    ATOMIC_DB: bool = Field(default=True, frozen=True, repr=False)
+
+    # Redis Configuration
+    REDIS_HOST: SecretStr = Field(default="localhost", frozen=True, repr=False)
+    REDIS_PORT: int = Field(default=6379, frozen=True, repr=False)
+
+    # Data base choices
+    DATABASE_CHOICES: str = Field(
+        default=DatabaseChoices.SQLITE,
+        frozen=True,
+        repr=False,
+    )
+
+    model_config = SettingsConfigDict(
+        env_file=env_config.env_file,
+        extra="ignore",
+        case_sensitive=True,
+    )
+
+    @computed_field()
+    def DATABASES(self) -> dict[str, Any]:
+        if self.DATABASE_CHOICES == "sqlite3":
+            return {
+                "default": {
+                    "ENGINE": "django.db.backends.sqlite3",
+                    "NAME": "db.sqlite3",
+                }
+            }
+        elif self.DATABASE_CHOICES == "mysql":
+            # TODO: Implement MySQL database configuration
+            pass
+        elif self.DATABASE_CHOICES == "postgres":
+            return {
+                "default": {
+                    "ENGINE": "django.db.backends.postgresql",
+                    "NAME": self.POSTGRES_DB.get_secret_value(),
+                    "USER": self.POSTGRES_USER.get_secret_value(),
+                    "PASSWORD": self.POSTGRES_PASSWORD.get_secret_value(),
+                    "HOST": self.POSTGRES_HOST.get_secret_value(),
+                    "PORT": self.POSTGRES_PORT,
+                },
+            }
+
+
+db_config = DatabaseSettings()
